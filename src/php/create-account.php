8afdaae9e0603ab62e7a3083e04c14fd5ee594ff
wolfiy <wolfiy@protonmail.ch>
2024-06-07 13:17:03 +0200

    Create account / signin

--- src/php/create-account.php
+++ src/php/create-account.php
@@ -10,23 +10,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     error_log("Attempting to add new account...");
 
-    // TODO js
     if ($password != $passwordConfirm) {
         header("Location: ../html/create-account.html?error=password_mismatch");
         exit();
     }
     
     try {
-        // TODO
-        $password = password_hash($password);
+        $password = password_hash($password, PASSWORD_BCRYPT);
         $sql = "INSERT INTO t_Account (accEmail, accUsername, accPassword, accAge) 
                 VALUES (?, ?, ?, ?)";
         $stmt = $conn->prepare($sql);
         $stmt->execute([$email, $username, $password, $age]);
         error_log("Account successfully created.");
     } catch (Exception $e) {
-        throw new Exception("Account could not be created." . $e -> getMessage());
-        //header("Location: ../html/create-account.html?error=db_error");
+        error_log("Account could not be created." . $e -> getMessage());
+        header("Location: ../html/create-account.html?error=db_error");
         exit();
     }
 
