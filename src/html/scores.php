d85c2cac96520badc86e41b799df70cbc31477b3
wolfiy <wolfiy@protonmail.ch>
2024-06-11 11:19:42 +0200

    Merged leaderboard

--- src/html/scores.php
+++ src/html/scores.php
@@ -1,5 +1,5 @@
 <?php
-  session_start();
+  require '../scripts/session-check.php';
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +8,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../styles/main.css">
-  <link rel="stylesheet" href="../styles/index.css">
-  <title>Accueil</title>
+  <link rel="stylesheet" href="../styles/scores.css">
+  <title>Classement</title>
 </head>
 <body>
   <header>
@@ -48,18 +48,36 @@
 
   <main>
     <div class="content">
-      <h1>Bienvenue sur GreenHabits</h1>
-
-      <div id="home-mascots-container">
-        <img src="../../assets/eco-lojie.png" 
-             alt="Eco et Lojie, les mascots de GreenHabits"
-             id="home-mascots">
-  
-        <p id="home-mascots-text">
-          Salut! Nous c'est Eco et Lojie! Es-tu prêt à améliorer tes habitudes écologiques avec nous?
-        </p>
+      <h1>Classement</h1>
+      
+      <div class="leaderboard">
+        <h2>Placeholder</h2>
+        <div class="leaderboard-item">
+          <div class="rank rank-1">1</div>
+          <div class="leaderboard-username">Moi</div>
+          <div class="points">50 Pts</div>
+        </div>
+        <div class="leaderboard-item">
+          <div class="rank rank-2">2</div>
+          <div class="leaderboard-username">Annabelle</div>
+          <div class="points">40 Pts</div>
+        </div>
+        <div class="leaderboard-item">
+          <div class="rank rank-3">3</div>
+          <div class="leaderboard-username">Nolan</div>
+          <div class="points">35 Pts</div>
+        </div>
+        <div class="leaderboard-item">
+          <div class="rank rank-4">4</div>
+          <div class="leaderboard-username">Sophie</div>
+          <div class="points">15 Pts</div>
+        </div>
+        <div class="leaderboard-item">
+          <div class="rank rank-5">5</div>
+          <div class="leaderboard-username">Laurent</div>
+          <div class="points">5 Pts</div>
+        </div>
       </div>
-
     </div>
   </main>
 
